7524baf6879a59c3a1ff42ea25dfb650c1fce53d
Gerasimovich Igor <mgorunuch.igor@gmail.com>
2024-10-10 19:36:10 +0100

    fix

--- iterator_slice.go
+++ iterator_slice.go
@@ -1,17 +1,13 @@
 package gosuper
 
-// NewSliceIterator creates a new SliceIterator for the given slice.
 func NewSliceIterator[Type any](slice []Type) *SliceIterator[Type] {
 	return &SliceIterator[Type]{slice: slice}
 }
 
-// SliceIterator is an iterator implementation for slices.
 type SliceIterator[Type any] struct {
 	slice []Type
 }
 
-// Next returns the next element in the slice.
-// If there are no more elements, it returns the zero value for Type and ErrStopIteration.
 func (iter *SliceIterator[Type]) Next() (t Type, err error) {
 	if len(iter.slice) == 0 {
 		return t, ErrStopIteration
--- main.go
+++ main.go
@@ -1,4 +1,1 @@
-// Package gosuper provides a collection of generic iterator implementations and utilities.
-// It includes iterators for slices, readers, and separated readers, as well as interfaces
-// for creating custom iterators.
 package gosuper
